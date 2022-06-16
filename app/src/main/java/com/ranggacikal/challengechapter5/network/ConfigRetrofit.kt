c68247276abc2209c5553593e5b476ff4b3785d5
Rangga Cikal <ranggacikal2@gmail.com>
2022-06-16 13:46:37 +0700

    add header in retrofit builder

--- app/src/main/java/com/ranggacikal/challengechapter5/network/ConfigRetrofit.kt
+++ app/src/main/java/com/ranggacikal/challengechapter5/network/ConfigRetrofit.kt
@@ -2,11 +2,11 @@ package com.ranggacikal.challengechapter5.network
 
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 object ConfigRetrofit {
 
@@ -17,14 +17,17 @@ object ConfigRetrofit {
     }
 
     private fun provideOkHttpClient(): OkHttpClient {
-        val builder = OkHttpClient.Builder()
-            .addInterceptor(provideLoggingInterceptor())
-            .retryOnConnectionFailure(false)
-            .callTimeout(1, TimeUnit.MINUTES)
-            .connectTimeout(1, TimeUnit.MINUTES)
-            .readTimeout(1, TimeUnit.MINUTES)
-
-        return builder.build()
+        return OkHttpClient.Builder().apply {
+            addInterceptor(
+                Interceptor { chain ->
+                    val builder = chain.request().newBuilder()
+                    builder.header("Test-App-Version", "1.0")
+                    builder.header("X-Platform", "Android")
+                    builder.header("X-Auth-Token", "sgsrager32524542afg3423")
+                    return@Interceptor chain.proceed(builder.build())
+                }
+            )
+        }.build()
     }
 
     private fun provideRetrofit(): Retrofit {
